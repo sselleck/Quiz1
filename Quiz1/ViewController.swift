dc5e991ea966a1523abf4661bf634f2f77c59b50
Samantha Selleck <samanthaselleck@Samanthas-MacBook-Pro.local>
2017-02-21 14:29:41 -0500

    Completed up till Chapter 7

--- Quiz1/ViewController.swift
+++ Quiz1/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet var answerLabel: UILabel!
     
     let questions: [String] = [
@@ -31,8 +34,9 @@ class ViewController: UIViewController {
         }
         
         let question: String = questions[currentQuestionIndex]
-        questionLabel.text = question
+        nextQuestionLabel.text = question
         answerLabel.text = "???"
+        animateLabelTransitions()
     }
 
     @IBAction func showAnswer(_ sender: UIButton) {
@@ -42,7 +46,66 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = questions[currentQuestionIndex]
+        currentQuestionLabel.text = questions[currentQuestionIndex]
+        
+        updateOffScreenLabel()
+        
+        /*
+        currentQuestionLabel.text = questions[currentQuestionIndex]
+ 
+        // Disable current centerX constraint of nextQuestionLabel
+        nextQuestionLabelCenterXConstraint.isActive = false
+ 
+        // Create a UILayoutGuide instance between the centerXs of the two labels
+        let spaceBetweenLabels = UILayoutGuide()
+        self.view.addLayoutGuide(spaceBetweenLabels)
+        spaceBetweenLabels.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        nextQuestionLabel.centerXAnchor.constraint(equalTo: spaceBetweenLabels.leadingAnchor).isActive = true
+        currentQuestionLabel.centerXAnchor.constraint(equalTo: spaceBetweenLabels.trailingAnchor).isActive = true
+        */
+    }
+ 
+    func updateOffScreenLabel(){
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
+    }
+    func animateLabelTransitions(){
+        //Force any outstanding layout changes to occur
+        view.layoutIfNeeded()
+        
+        //Animate the alpha
+        //and the center x constraints 
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+        
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       options: [.curveLinear],
+                       animations:{
+                            self.currentQuestionLabel.alpha = 0
+                            self.nextQuestionLabel.alpha = 1
+                            self.view.layoutIfNeeded()
+                       },
+                       completion: { _ in
+                        swap(&self.currentQuestionLabel,
+                             &self.nextQuestionLabel)
+                        swap(&self.currentQuestionLabelCenterXConstraint,
+                             &self.nextQuestionLabelCenterXConstraint)
+                        //self.currentQuestionLabel.text = self.nextQuestionLabel.text
+                        //self.currentQuestionLabel.alpha = 1
+                        //self.currentQuestionLabelCenterXConstraint.constant = 0
+                        //self.view.layoutIfNeeded()
+                        //self.nextQuestionLabel.alpha = 0
+                        self.updateOffScreenLabel()
+        })
+    }
+    
+    override func viewWillAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
+        
+        //Set the label's initial alpha
+        nextQuestionLabel.alpha = 0
     }
 }
 
